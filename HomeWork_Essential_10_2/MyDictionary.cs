68fc9d2e7a114efe68a5c47e60c7c19a7f57d56c
AnastasiiaVV <vitchenkoanastasiia93@gmail.com>
2021-07-21 23:30:15 +0300

    Home work c# essential lesson 10 (last part)

---
+++ HomeWork_Essential_10_2/MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_Essential_10_2
+{
+	class MyDictionary <TKey, TValue>
+	{
+		private TKey[] _key = new TKey[10];
+		private TValue[] _value = new TValue[10];
+        public int Length { get; private set; }
+        public MyDictionary() {
+        }
+
+        public TValue this[TKey index] {
+            get {
+                for (int i = 0; i < _key.Length && i < Length; i++) {
+                    if (_key[i].Equals(index)) {
+                        return _value[i];
+                    }
+                }
+                throw new KeyNotFoundException("Нет такого варианта!");
+            }
+        }
+
+        public TValue this[int index] {
+            get {
+                if (index >= 0 && index < _key.Length && index < Length) {
+                    return _value[index];
+                }
+                throw new IndexOutOfRangeException("Выход за пределы массива!");
+            }
+        }
+
+        public void Add(TKey key, TValue value) {
+            _key[Length] = key;
+            _value[Length] = value;
+            Length++;
+        }
+
+    }
+}
---
+++ HomeWork_Essential_10_3/MyListExtention.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_Essential_10_3
+{
+	static class MyListExtention
+	{
+		public static T[] GetArray<T>(this MyList<T> list) {
+			T[] array = new T[list.Count];
+			for (int i = 0; i < list.Count; i++) {
+				array[i] = list[i];
+			}
+			return array;
+
+		}
+
+	}
+}
